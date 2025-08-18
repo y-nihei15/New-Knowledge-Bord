278f9132d1dbee8b732452d6f7335eec82bdae25
Yoneda Noa <n-yoneda@know-net.co.jp>
2025-08-18 14:14:35 +0900

    【fix】db.phpのコードミスを修正しました。

--- top_api/config/db.php
+++ top_api/config/db.php
@@ -3,9 +3,10 @@ function getDbConnection() {
     $dsn = 'mysql:dbname=attendance_board;host=133.18.236.157';
     $user = 'root';
     $password = 'Knowledge88!!';
+ 
 
     try {
-        $pdo = new PDO("dbn=$dsn;charset=utf8mb4", $user, $password);
+        $pdo = new PDO("$dsn;charset=utf8mb4", $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
@@ -14,6 +15,3 @@ function getDbConnection() {
         exit;
     }
 }
-
-
-
